b6d7cfee34a5d74dee0e05e319fe509986afd744
Jorge De Jesus Rivera Perez <i20163mc061@utez.edu.mx>
2021-12-11 13:57:03 -0600

    get record from localStorage

--- src/app/services/historial.service.ts
+++ src/app/services/historial.service.ts
@@ -11,10 +11,18 @@ export class HistorialService {
     return [...this._historal];
   }
 
+  constructor(){
+
+    this._historal = JSON.parse(localStorage.getItem('urlHistorial')!) || [];
+
+  }
+
   insertarHistorial(value: string){
 
     if(!this._historal.includes(value)){
       this._historal.unshift(value);
+
+      localStorage.setItem('urlHistorial', JSON.stringify(this._historal));
     }
     this._historal = this._historal.splice(0,10);
   };
