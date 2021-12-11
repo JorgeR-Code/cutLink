6644c6144a7fadb27106175d5955d9f6633f0b14
Jorge De Jesus Rivera Perez <i20163mc061@utez.edu.mx>
2021-12-11 11:17:30 -0600

    Error message

--- src/app/pages/input/input.component.ts
+++ src/app/pages/input/input.component.ts
@@ -1,26 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
 import { CutUrlService } from 'src/app/cut-url.service';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+  styleUrls: ['./input.component.scss'],
+  providers: [MessageService]
 })
 export class InputComponent implements OnInit {
 
   input: string = '';
-  constructor(private urlServiceValue: CutUrlService) { }
+
+
+
+
+  constructor(private urlServiceValue: CutUrlService, private messageService: MessageService) { }
 
   ngOnInit(): void {
+
   }
 
 
   value(url: string){
-    if(url){
+
+    const control = new FormControl(url, Validators.pattern('https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}'));
+
+    console.log(control.errors);
+    if(url && !control.errors){
       this.urlServiceValue.cutUrl(url);
       this.input = '';
     }else{
+      this.showError();
       return
     }
   }
+  showError() {
+    this.messageService.add({key: 'tc', severity:'error', summary: 'URL no válida'});
+  }
+
 }
--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -4,6 +4,9 @@ import {InputTextModule} from 'primeng/inputtext';
 import {ButtonModule} from 'primeng/button';
 import {FieldsetModule} from 'primeng/fieldset';
 import {SidebarModule} from 'primeng/sidebar';
+import {MessagesModule} from 'primeng/messages';
+import {MessageModule} from 'primeng/message';
+import {ToastModule} from 'primeng/toast';
 
 @NgModule({
   declarations: [],
@@ -15,7 +18,10 @@ import {SidebarModule} from 'primeng/sidebar';
     InputTextModule,
     ButtonModule,
     FieldsetModule,
-    SidebarModule
+    SidebarModule,
+    MessagesModule,
+    MessageModule,
+    ToastModule
   ]
 })
 export class PrimeNgModule { }
